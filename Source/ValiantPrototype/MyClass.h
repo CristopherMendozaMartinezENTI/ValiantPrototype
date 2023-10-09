a61e03d5cbda29ae1e1a8f1f3fd6d9e8a972122b
Pau Marin <paumarin96@gmail.com>
2023-10-09 12:26:23 +0200

    converted to cpp

---
+++ Source/ValiantPrototype/MyClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class VALIANTPROTOTYPE_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+};
