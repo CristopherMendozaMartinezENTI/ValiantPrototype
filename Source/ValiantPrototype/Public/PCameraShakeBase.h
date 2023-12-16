6609a27344117c3e29edf785fb0b891be282fee3
paumarin96 <37964922+paumarin96@users.noreply.github.com>
2023-12-16 19:21:22 +0100

    wip

---
+++ Source/ValiantPrototype/Public/PCameraShakeBase.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraShakeBase.h"
+#include "PCameraShakeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VALIANTPROTOTYPE_API UPCameraShakeBase : public UCameraShakeBase
+{
+	GENERATED_BODY()
+
+	void UpdateScale()
+	{
+		
+	}
+	
+};
